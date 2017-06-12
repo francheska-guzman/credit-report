1f5a76bf1c48e6bb479469344fcb2ea8ee14c1a0
gffrancheska <gffrancheska@gmail.com>
2017-06-12 15:21:29 -0400

    Adding links in index for the API

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var db = require('../queries');
 
-router.get('/', db.getAllUserInformation);
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Credit Report API' });
+})
+
+router.get('/getalluserinformation', db.getAllUserInformation);
 
 module.exports = router;
