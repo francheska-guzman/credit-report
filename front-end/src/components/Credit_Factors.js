5be58d21543d017803529e11846faf8c2c72300f
gffrancheska <gffrancheska@gmail.com>
2017-06-13 16:41:48 -0400

    I can target the button.

--- front-end/src/components/Credit_Factors.js
+++ front-end/src/components/Credit_Factors.js
@@ -7,6 +7,17 @@ import PaymentHistory from './creditfactors/Payment_History';
 import TotalAccounts from './creditfactors/Total_Accounts';
 
 class CreditFactors extends Component {
+  constructor(){
+    super();
+    this.state = {
+    display: false
+    }
+  }
+
+  showData(event) {
+    console.log(event.target.id);
+  }
+
   render() {
     return (
       <div className="credit-factors">
@@ -14,30 +25,30 @@ class CreditFactors extends Component {
         <div className="flex">
           <div className="box flex-1">
           <h3>Credit Age</h3>
-          <input type="button" className="text-appearance" value="Details" onClick=""/>
+          <input type="button" id="creditfactor1" className="text-appearance" value="Details" onClick={this.showData} />
           </div>
   	      <div className="box flex-1">
           <h3>Credit Card Use</h3>
-          <input type="button" className="text-appearance" value="Details" onClick=""/>
+          <input type="button" id="creditfactor2" className="text-appearance" value="Details" onClick={this.showData} />
           </div>
           <div className="box flex-1">
           <h3>Derogatory Marks</h3>
-          <input type="button" className="text-appearance" value="Details" onClick=""/>
+          <input type="button" id="creditfactor3" className="text-appearance" value="Details" onClick={this.showData} />
           </div>
         </div>
 
         <div className="flex">
   	      <div className="box flex-1">
           <h3>Hard Inquiries</h3>
-          <input type="button" className="text-appearance" value="Details" onClick=""/>
+          <input type="button" id="creditfactor4" className="text-appearance" value="Details" onClick={this.showData} />
           </div>
           <div className="box flex-1">
           <h3>Payment History</h3>
-          <input type="button" className="text-appearance" value="Details" onClick=""/>
+          <input type="button" id="creditfactor5" className="text-appearance" value="Details" onClick={this.showData} />
           </div>
   	      <div className="box flex-1">
           <h3>Total Accounts</h3>
-          <input type="button" className="text-appearance" value="Details" onClick=""/>
+          <input type="button" id="creditfactor6" className="text-appearance" value="Details" onClick={this.showData} />
           </div>
 	      </div>
 	    </div>
