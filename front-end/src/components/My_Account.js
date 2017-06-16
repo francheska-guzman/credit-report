e45b33032529ab9f667ddee4a15773566917ebe4
gffrancheska <gffrancheska@gmail.com>
2017-06-15 22:34:21 -0400

    Updating

--- front-end/src/components/My_Account.js
+++ front-end/src/components/My_Account.js
@@ -16,13 +16,27 @@ class MyAccount extends Component {
   	else {
   	  return (
         <div className="profile flex">
-          <ul className="profile-wrap">
+          <ul className="profile-wrap flex-1">
+          <h2>Name</h2>
           <li className="profile-info">First Name: {this.props.state.first_name}</li>
+          <li className="profile-info">Middle Name: </li>
           <li className="profile-info">Last Name: {this.props.state.last_name}</li>
+          <h2>About You</h2>
           <li className="profile-info">Date of Birth: {this.props.state.date_of_birth}</li>
           <li className="profile-info">Place of Birth: {this.props.state.place_of_birth}</li>
           <li className="profile-info">Marital Status: {this.props.state.marital_status}</li>
           <li className="profile-info">Annual Household Income: {this.props.state.annual_household_income}</li>
+          </ul>
+          <ul className="profile-wrap flex-1">
+          <h2>Home Address</h2>
+          <li className="profile-info">Street Address: </li>
+          <li className="profile-info">Apartment: </li>
+          <li className="profile-info">City: </li>
+          <li className="profile-info">State: </li>
+          <li className="profile-info">Zip Code: </li>
+          <h2>Contact</h2>
+          <li className="profile-info">Telephone: </li>
+          <li className="profile-info">Email: </li>
           </ul>
         </div>
   	  );
