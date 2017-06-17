b6aa26cbac37b5cbd8c50661ab71e6fc248ba28b
gffrancheska <gffrancheska@gmail.com>
2017-06-17 12:50:39 -0400

    Adding comments.

--- front-end/src/components/Credit_Card_Use.js
+++ front-end/src/components/Credit_Card_Use.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Doughnut } from 'react-chartjs';
-import UtilizationTable from './UtilizationTable';
+import UtilizationTable from './Utilization_Table';
 
 class CreditCardUse extends Component {
 constructor(props){
@@ -8,44 +8,47 @@ constructor(props){
   this.state = {
     counter_use: 0
   }
-  this.useRatio = this.useRatio.bind(this);
+  this.utilizationRatio = this.utilizationRatio.bind(this);
   this.openCreditCards = this.openCreditCards.bind(this);
   this.headerUse = this.headerUse.bind(this);
   }
 
-  useRatio() {
+  utilizationRatio() {
     var use = 0;
     var limit = 0;
     var counter = 0;
 
     for(var i = 0; i < this.props.state.account_type.length; i += 1) {
     // If the account type is a credit card, and is currently open:
-      if (this.props.state.account_type[i] === "Credit Card" && 
-        this.props.state.current_status[i] === "Open") {
+      if (this.props.state.account_type[i] === "Credit Card" && this.props.state.current_status[i] === "Open") {
           use += this.props.state.credit_use[i];
           limit += this.props.state.credit_limit[i];
           counter += 1;
-      }
-    }
+    }};
 
     this.setState({
       counter_use: counter
-    })
+    });
 
+    // If user have 0 or 1 credit card render singular, otherwise render in plural:
     var creditcard = ((counter <= 1) ? "credit card" : "credit cards");
+    // To avoid fractions:
     var ratio = (use/limit).toFixed(2) * 100;
+    // Calculate the available credit:
     var available = limit - use;
 
-    let data = [
+    // Data for the Doughnut chart:
+    var data = [
             {color: "#C1091f", label: "Credit Use", value: use},
             {color: "#0E9E17", label: "Available Credit", value: available}
     ];
 
+    // Using an if/else statement to display a color for a certain ratio range.
   	if(ratio <= 9) {
   	  return (
         <div className="flex">
           <div className="flex-1 center">
-            <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
           </div>
           <div className="flex-1">
             <ul className="factor-info">
@@ -64,7 +67,7 @@ constructor(props){
   	  return (
         <div className="flex">
           <div className="flex-1 center">
-            <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
           </div>
           <div className="flex-1">
             <ul className="factor-info">
@@ -83,7 +86,7 @@ constructor(props){
   	  return (
         <div className="flex">
           <div className="flex-1 center">
-            <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
           </div>
           <div className="flex-1">
             <ul className="factor-info">
@@ -102,7 +105,7 @@ constructor(props){
   	  return (
         <div className="flex">
           <div className="flex-1 center">
-            <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
           </div>
           <div className="flex-1">
             <ul className="factor-info">
@@ -121,7 +124,7 @@ constructor(props){
   	  return (
         <div className="flex">
           <div className="flex-1 center">
-            <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
           </div>
           <div className="flex-1">
             <ul className="factor-info">
@@ -138,7 +141,7 @@ constructor(props){
 
     else {
       return (
-        <div>You have 0 open credit card.</div>
+        <div className="flex">You have 0 open credit card.</div>
     )}
   }
 
@@ -146,16 +149,19 @@ constructor(props){
     var table = [];
 
     for(var i = 0; i < this.props.state.opened.length; i += 1) {
-      if (this.props.state.account_type[i] === "Credit Card" && 
-          this.props.state.current_status[i] === "Open") { 
-          table.push([<td>{this.props.state.opened[i]}</td>,<td>{this.props.state.creditor[i]}</td>,
-            <td>{this.props.state.credit_limit[i]}</td>,<td>{this.props.state.credit_use[i]}</td>,
-            <td>{this.props.state.current_status[i]}</td>]);
+      if (this.props.state.account_type[i] === "Credit Card" && this.props.state.current_status[i] === "Open") { 
+          table.push([
+            <td>{this.props.state.opened[i]}</td>,
+            <td>{this.props.state.creditor[i]}</td>,
+            <td>{this.props.state.credit_limit[i]}</td>,
+            <td>{this.props.state.credit_use[i]}</td>,
+            <td>{this.props.state.current_status[i]}</td>
+          ]);
         }
       }
-      return (table.map(function(table, i){
-        return <tr key={i+1}>{table}</tr>
-     }))
+      return (table.map(function(table, n){
+        return (<tr key={n+1}>{table}</tr>)
+     }));
   }
 
   headerUse() {
@@ -169,7 +175,7 @@ constructor(props){
         <th className="t-title">Credit Use</th>
         <th className="t-title">Current Status</th>
       </tr>
-    )}}
+    )}};
   }
 
   render() {
@@ -180,7 +186,7 @@ constructor(props){
           percentage of your credit limits that you're using. It's calculated 
           by dividing your total credit balances by your total credit limits on open accounts.</span><br /><br />
           <span className="factor-info">A good rule of thumb is to keep your balances below 30% of your limit.</span><br /><br />
-          <span className="graphic-description flex-5">{this.useRatio()}</span>
+          <span className="graphic-description flex-5">{this.utilizationRatio()}</span>
             <table className="dark">
               <tbody>
               {this.headerUse()}
