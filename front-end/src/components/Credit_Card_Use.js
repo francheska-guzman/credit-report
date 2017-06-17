26a8b08dbe8774b9f76fb0524922f50cb7ae463b
gffrancheska <gffrancheska@gmail.com>
2017-06-17 15:14:38 -0400

    Working in payment history

--- front-end/src/components/Credit_Card_Use.js
+++ front-end/src/components/Credit_Card_Use.js
@@ -5,9 +5,6 @@ import UtilizationTable from './Utilization_Table';
 class CreditCardUse extends Component {
 constructor(props){
   super(props);
-  this.state = {
-    counter_use: 0
-  }
   this.utilizationRatio = this.utilizationRatio.bind(this);
   this.openCreditCards = this.openCreditCards.bind(this);
   this.headerUse = this.headerUse.bind(this);
@@ -16,7 +13,7 @@ constructor(props){
   utilizationRatio() {
     var use = 0;
     var limit = 0;
-    var counter = 0;
+    var counter = null;
 
     for(var i = 0; i < this.props.state.account_type.length; i += 1) {
     // If the account type is a credit card, and is currently open:
@@ -25,10 +22,6 @@ constructor(props){
           limit += this.props.state.credit_limit[i];
           counter += 1;
     }};
-
-    this.setState({
-      counter_use: counter
-    });
 
     // If user have 0 or 1 credit card render singular, otherwise render in plural:
     var creditcard = ((counter <= 1) ? "credit card" : "credit cards");
@@ -39,103 +32,103 @@ constructor(props){
 
     // Data for the Doughnut chart:
     var data = [
-            {color: "#C1091f", label: "Credit Use", value: use},
-            {color: "#0E9E17", label: "Available Credit", value: available}
+      {color: "#C1091f", label: "Credit Use", value: use},
+      {color: "#0E9E17", label: "Available Credit", value: available}
     ];
 
     // Using an if/else statement to display a color for a certain ratio range.
   	if(ratio <= 9) {
   	  return (
         <div className="flex">
-          <div className="flex-1 center">
-          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
-          </div>
+        <div className="flex-1 center">
+        <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+        </div>
           <div className="flex-1">
             <ul className="factor-info">
-    	        <li className="ratio green">{ratio}%</li>
-              <li>You have {counter} open {creditcard}.</li>
-              <li>Your total credit limit is: {limit}</li>
-              <li>Your total credit use is: {use}</li>
-              <li>Your total available credit is: {available}</li>
+    	      <li className="ratio green">{ratio}%</li>
+            <li>You have {counter} open {creditcard}.</li>
+            <li>Your total credit limit is: {limit}</li>
+            <li>Your total credit use is: {use}</li>
+            <li>Your total available credit is: {available}</li>
             </ul>
           </div>
-          <UtilizationTable />
+        <UtilizationTable />
         </div>
   	)}
 
   	else if(ratio <= 29) {
-  	  return (
+      return (
         <div className="flex">
-          <div className="flex-1 center">
-          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
-          </div>
+        <div className="flex-1 center">
+        <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+        </div>
           <div className="flex-1">
             <ul className="factor-info">
-              <li className="ratio green">{ratio}%</li>
-              <li>You have {counter} open {creditcard}.</li>
-              <li>Your total credit limit is: {limit}</li>
-              <li>Your total credit use is: {use}</li>
-              <li>Your total available credit is: {available}</li>
+            <li className="ratio green">{ratio}%</li>
+            <li>You have {counter} open {creditcard}.</li>
+            <li>Your total credit limit is: {limit}</li>
+            <li>Your total credit use is: {use}</li>
+            <li>Your total available credit is: {available}</li>
             </ul>
           </div>
-          <UtilizationTable />
+        <UtilizationTable />
         </div>
   	)}
 
   	else if(ratio <= 49) {
-  	  return (
+      return (
         <div className="flex">
-          <div className="flex-1 center">
-          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
-          </div>
+        <div className="flex-1 center">
+        <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+        </div>
           <div className="flex-1">
             <ul className="factor-info">
-              <li className="ratio yellow">{ratio}%</li>
-              <li>You have {counter} open {creditcard}.</li>
-              <li>Your total credit limit is: {limit}</li>
-              <li>Your total credit use is: {use}</li>
-              <li>Your total available credit is: {available}</li>
+            <li className="ratio yellow">{ratio}%</li>
+            <li>You have {counter} open {creditcard}.</li>
+            <li>Your total credit limit is: {limit}</li>
+            <li>Your total credit use is: {use}</li>
+            <li>Your total available credit is: {available}</li>
             </ul>
           </div>
-          <UtilizationTable />
+        <UtilizationTable />
         </div>
   	)}
 
   	else if(ratio <= 74) {
-  	  return (
+      return (
         <div className="flex">
-          <div className="flex-1 center">
-          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
-          </div>
+        <div className="flex-1 center">
+        <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+        </div>
           <div className="flex-1">
             <ul className="factor-info">
-              <li className="ratio red">{ratio}%</li>
-              <li>You have {counter} open {creditcard}.</li>
-              <li>Your total credit limit is: {limit}</li>
-              <li>Your total credit use is: {use}</li>
-              <li>Your total available credit is: {available}</li>
+            <li className="ratio red">{ratio}%</li>
+            <li>You have {counter} open {creditcard}.</li>
+            <li>Your total credit limit is: {limit}</li>
+            <li>Your total credit use is: {use}</li>
+            <li>Your total available credit is: {available}</li>
             </ul>
           </div>
-          <UtilizationTable />
+        <UtilizationTable />
         </div>
   	)}
 
   	else if(ratio <= 100) {
-  	  return (
+      return (
         <div className="flex">
-          <div className="flex-1 center">
-          <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
-          </div>
+        <div className="flex-1 center">
+        <Doughnut data={data} options={{animateRotate: true}} width="250" height="250"/>
+        </div>
           <div className="flex-1">
             <ul className="factor-info">
-              <li className="ratio red">{ratio}%</li>
-              <li>You have {counter} open {creditcard}.</li>
-              <li>Your total credit limit is: {limit}</li>
-              <li>Your total credit use is: {use}</li>
-              <li>Your total available credit is: {available}</li>
+            <li className="ratio red">{ratio}%</li>
+            <li>You have {counter} open {creditcard}.</li>
+            <li>Your total credit limit is: {limit}</li>
+            <li>Your total credit use is: {use}</li>
+            <li>Your total available credit is: {available}</li>
             </ul>
           </div>
-          <UtilizationTable />
+        <UtilizationTable />
         </div>
   	)}
 
@@ -151,22 +144,21 @@ constructor(props){
     for(var i = 0; i < this.props.state.opened.length; i += 1) {
       if (this.props.state.account_type[i] === "Credit Card" && this.props.state.current_status[i] === "Open") { 
           table.push([
-            <td>{this.props.state.opened[i]}</td>,
-            <td>{this.props.state.creditor[i]}</td>,
-            <td>{this.props.state.credit_limit[i]}</td>,
-            <td>{this.props.state.credit_use[i]}</td>,
-            <td>{this.props.state.current_status[i]}</td>
+            <td key={"Opened_"+i+1}>{this.props.state.opened[i]}</td>,
+            <td key={"Creditor_"+i+1}>{this.props.state.creditor[i]}</td>,
+            <td key={"CreditLimit_"+i+1}>{this.props.state.credit_limit[i]}</td>,
+            <td key={"CreditUse_"+i+1}>{this.props.state.credit_use[i]}</td>,
+            <td key={"CurrentStatus_"+i+1}>{this.props.state.current_status[i]}</td>
           ]);
         }
       }
       return (table.map(function(table, n){
-        return (<tr key={n+1}>{table}</tr>)
-     }));
+        return (
+          <tr key={n+1}>{table}</tr>
+      )}));
   }
 
   headerUse() {
-    for(var i = 0; i < this.props.state.collection.length; i += 1){
-    if(this.state.counter_use > 0){
     return (
       <tr>
         <th className="t-title">Opened</th>
@@ -175,8 +167,7 @@ constructor(props){
         <th className="t-title">Credit Use</th>
         <th className="t-title">Current Status</th>
       </tr>
-    )}};
-  }
+  )};
 
   render() {
     return (
--- front-end/src/components/Payment_History.js
+++ front-end/src/components/Payment_History.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
 
 class PaymentHistory extends Component {
+  constructor(props){
+  super(props);
+  this.state = {
+  	accounts_ph: []
+  }
+  this.componentWillMount = this.componentWillMount.bind(this);
+  this.tableOfPayments = this.tableOfPayments.bind(this);
+  }
+
+  // Looping though payments to add color.
+  componentWillMount() {
+  // Local variable.
+  let accounts = [];
+
+  	// Looping through each account.
+	for (var i = 0; i < this.props.state.payment_history.length; i += 1) {
+    // Everytime the second for loop ends, single_account starts in an empty array,
+    // to be used in the nex account.
+	let single_account = [];
+	  // Looping through each month of each account.
+	  for(var n = 0; n < this.props.state.payment_history[i].length; n += 1) {
+	  //console.log("Account: " + i + ", Payments for the Month: " + n + ": " +this.props.state.payment_history[i][n]);
+	  	// If user made a payment (true), render green color.
+	  	if(this.props.state.payment_history[i][n] === "true") {
+	  	single_account.push('<span className="green-background"></span>')
+	  	}
+	  	// Otherwise, render red color.
+	  	else {
+	  	single_account.push('<span className="red-background"><span>')
+		}
+	  }
+	  // When the 12 months finish, then push to this other array.
+	  accounts.push(single_account)
+    }
+    this.setState({
+      accounts_ph: accounts
+    }, function() {
+        this.tableOfPayments();
+    });
+  }
+
+  tableOfPayments(){
+  	console.log(this.state.accounts_ph)
+  }
+
   render() {
     return (
       <div id="payment-history" className="flex-1">
-        <h4 className="center purple">Payment History</h4>
+      <div className="center">
+        <h4 className="purple">Payment History</h4>
+      </div>
+        <table className="dark">
+     	<tbody>
+	      <tr>
+	      	<th>Creditor</th>
+	      	<th>Credit Limit</th>
+	      	<th>Credit Use</th>
+	      	<th>Current Status</th>
+	      	<th>Payment History</th>
+	      </tr>
+		  {this.tableOfPayments()}
+	    </tbody>
+        </table>
       </div>
     );
   }
