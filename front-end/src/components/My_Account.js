8defe967a3d23d80c6a15ea94e2ba1d046a725a9
gffrancheska <gffrancheska@gmail.com>
2017-06-18 19:33:27 -0400

    Updating

--- front-end/src/components/My_Account.js
+++ front-end/src/components/My_Account.js
@@ -14,30 +14,28 @@ class MyAccount extends Component {
   	else {
   	  return (
         <div className="profile flex">
-          <div className="profile-wrap flex-1">
+          <ul className="profile-wrap flex-1">
           <h2>Name</h2>
-            <label className="profile-info">First Name:</label><input type="text" className="profile-info" value={this.props.state.first_name} readonly/><br/>
-            <label className="profile-info">Middle Name:</label><input type="text" className="profile-info" value={this.props.state.middle_name} readonly/><br/>
-            <label className="profile-info">Last Name:</label><input type="text" className="profile-info" value={this.props.state.last_name} readonly/><br/>
+            <li className="profile-info">First Name: {this.props.state.first_name}</li>
+            <li className="profile-info">Middle Name: {this.props.state.middle_name}</li>
+            <li className="profile-info">Last Name: {this.props.state.last_name}</li>
           <h2>About You</h2>
-            <label className="profile-info">Date of Birth:</label><input type="text" className="profile-info" value={this.props.state.date_of_birth} readonly/><br/>
-            <label className="profile-info">Place of Birth:</label><input type="text" className="profile-info" value={this.props.state.place_of_birth} readonly/><br/>
-            <label className="profile-info">Marital Status:</label><input type="text" className="profile-info" value={this.props.state.marital_status} readonly/><br/>
-            <label className="profile-info">Annual Household Income:</label><input type="text" className="profile-info" value={this.props.state.annual_household_income} readonly/>
-          </div>
-
-          <div className="profile-wrap flex-1">
+            <li className="profile-info">Date of Birth: {this.props.state.date_of_birth}</li>
+            <li className="profile-info">Place of Birth: {this.props.state.place_of_birth}</li>
+            <li className="profile-info">Marital Status: {this.props.state.marital_status}</li>
+            <li className="profile-info">Annual Household Income: {this.props.state.annual_household_income}</li>
+          </ul>
+          <ul className="profile-wrap flex-1">
           <h2>Home Address</h2>
-            <label className="profile-info">Street Address:</label><input type="text" className="profile-info" value={this.props.state.street_address} readonly/><br/>
-            <label className="profile-info">Apartment:</label><input type="text" className="profile-info" value={this.props.state.apartment} readonly/><br/>
-            <label className="profile-info">City:</label><input type="text" className="profile-info" value={this.props.state.city} readonly/><br/>
-            <label className="profile-info">State:</label><input type="text" className="profile-info" value={this.props.state.state} readonly/><br/>
-            <label className="profile-info">Zip Code:</label><input type="text" className="profile-info" value={this.props.state.zip_code} readonly/>
+            <li className="profile-info">Street Address: {this.props.state.street_address}</li>
+            <li className="profile-info">Apartment: {this.props.state.apartment}</li>
+            <li className="profile-info">City: {this.props.state.city}</li>
+            <li className="profile-info">State: {this.props.state.state}</li>
+            <li className="profile-info">Zip Code: {this.props.state.zip_code}</li>
           <h2>Contact</h2>
-            <label className="profile-info">Telephone:</label><input type="text" className="profile-info" value={this.props.state.telephone_number} readonly/><br/>
-            <label className="profile-info">Email:</label><input type="text" className="profile-info" value={this.props.state.email} readonly/>
-          </div>
-          {this.renderbuttons}
+            <li className="profile-info">Telephone: {this.props.state.telephone_number}</li>
+            <li className="profile-info">Email: {this.props.state.email}</li>
+         </ul>
         </div>
   	  );
   	}
