46950ebee2cae4159b01037db3fa57a373638db2
gffrancheska <gffrancheska@gmail.com>
2017-06-18 12:51:40 -0400

    Updating

--- front-end/src/components/Credit_Score.js
+++ front-end/src/components/Credit_Score.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class CreditScore extends Component {
+  constructor(props){
+  	super(props);
+  	this.graphicAverage = this.graphicAverage.bind(this);
+  }
+
+  graphicAverage() {
+
+  }
+
   render() {
     return (
       <div>
         <h2 className="center">Credit Score</h2>
+        {this.graphicAverage()}
       </div>
     );
   }
